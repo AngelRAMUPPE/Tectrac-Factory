7e45052cc4f854ffc91b6f71d36bd5d01ec9f193
Angel Ruben Acosta Merino <114235440+AngelRAMUPPE@users.noreply.github.com>
2024-08-18 06:48:41 +0000

    Pagina maquinas actualizacion

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname + '/public')));
+
 
 //Ruta para servir la página HTML
 app.get('/', (req, res) => {
