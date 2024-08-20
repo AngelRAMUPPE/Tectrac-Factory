23323a65d23aecb4db2f7089c4c55fc3ccde0a42
Angel Ruben Acosta Merino <114235440+AngelRAMUPPE@users.noreply.github.com>
2024-08-20 09:02:59 -0600

    Actualizacion MongoDB

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express');
+const {MongoClient} = require('mongodb');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+
+const uri = 'mongodb+srv://admin:Contrase%C3%B1aChingona123@cluster0.3o5c1iv.mongodb.net/' //Dirección de conexión
+const dbname = 'Tectrac-Factory'; 
+const collectionName = 'Maquinas';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname + '/public')));
 
+//Ruta para manejar la solicitud de inserción de datps
+app.post('/insertar', async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    const client = new MongoClient(uri);
 
+    try{
+        client.connect();
+        const database = client.db(dbname);
+        const collection = database.collection(collectionName);
+        const result = await collection.insertOne(data);
+        console.log('Se insertó un documento con el ID: ' + result.insertedId);
+        res.send('Datos inseratados correctamente');
+    }
+    catch (error)
+    {
+        console.error('Error al insertar datos: ', error);
+        res.status(500).send('Error al insertar datos');
+    }
+    finally
+    {
+        await client.close();
+    }
+});
 //Ruta para servir la página HTML
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
